3545e6939b110e897a9c6aee6bde62fd1c8d44dd
Ali Kayhani <keyhani_a@yahoo.com>
2021-03-13 23:28:06 +0330

    home page and oop

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="author" content="SemiColonWeb"/>
+
+    <!-- Stylesheets
+	============================================= -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i"
+          rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'bootstrap.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'bootstrap-rtl.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'style.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'style-rtl.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'dark.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'dark-rtl.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'font-icons.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'font-icons-rtl.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'animate.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'magnific-popup.css' ) ?>" type="text/css"/>
+
+    <link rel="stylesheet" href="<?php echo asset::css( 'responsive.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'responsive-rtl.css' ) ?>" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo asset::css( 'fonts.css' ) ?>" type="text/css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<?php wp_head(); ?>
+    <!-- Document Title
+	============================================= -->
+
+</head>
+
+<body class="stretched">
